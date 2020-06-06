fa88a4c353a6662803c8983d26f41d97607196bf
Max Schipper <max.schipper@skola.botkyrka.se>
2020-06-06 18:12:27 +0200

    added type of wind

--- methods.py
+++ methods.py
@@ -19,3 +19,35 @@ def rain(rain):
         return "and remeber to bring a raincoat!"
     else:
         return ""
+
+def type_of_wind(wind):
+    if wind <= 0.5:
+        return "No wind"
+    elif 0.5 < wind <= 1.5:
+        return "Light air"
+    elif 1.5 < wind <= 3:
+        return "a Light breeze"
+    elif 3 < wind <= 5:
+        return "a Gentle breeze"
+    elif 5 < wind <= 8:
+        return "a Moderate breeze"
+    elif 8 < wind <= 10.5:
+        return "a Fresh breeze"
+    elif 10.5 < wind <= 13.5:
+        return "a Strong breeze"
+    elif 13.5 < wind <= 16.5:
+        return "a Moderate gale"
+    elif 16.5 < wind <= 20:
+        return "a Fresh gale"
+    elif 20 < wind <= 23.5:
+        return "a Strong gale"
+    elif 23.5 < wind <= 27.5:
+        return "a Whole gale"
+    elif 27.5< wind <= 31.5:
+        return "a Storm"
+    elif 31.5 < wind:
+        return "a Hurricane"
+
+print(type_of_wind(10))
+
+    
